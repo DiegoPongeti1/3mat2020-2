97a61e1282084ff9f9cebcda8099d15de408fc8a
DiegoPongeti1 <diego-p43@hotmail.com>
2020-09-10 12:07:56 +0000

    (10/09) Busca binaria em implementação recursiva

--- busca-binaria3.js
+++ busca-binaria3.js
@@ -1,12 +1,20 @@
 let comp = 0
-function buscaBinaria(lista, valorBusca) {
-    // Busca binária só funciona com uma lista preoordenada
-    let inicio = 0
-    let fim = lista.length - 1 
+// inicio = 0
+// fim = 12
 
-    while(fim > inicio) {
+// Implementação recursiva
+// Os parâmetros inicio e fim são opcionais, assumindo valores padrao
+// caso não tenham sido passados na chamada da função
+    function buscaBinaria(lista, valorBusca, fnComp, inicio = 0,
+         fim = lista.length - 1) {
+
+    if (fim > inicio) {
     // Math.floor() tira as casas decimais de um número
     let meio = Math.floor((fim + inicio) / 2)
+
+
+    let res = fnComp(lista[meio], valorBusca)
+
 
     if(res === 0 ) { // Achou
         comp++
@@ -17,14 +25,28 @@ function buscaBinaria(lista, valorBusca) {
         // antes do meio, assim descartando todos os valores
         // que são maiores que o valor na posição média
         comp += 2
-        fim = meio -1
+        // fim = meio -1
+      return  buscaBinaria(lista, valorBusca, fnComp, inicio, meio - 1)
     }
     else { // res > 0
         comp += 2
-        inicio = meio - 1
+        // inicio = meio - 1
+        return buscaBinaria(lista, valorBusca, fnComp, inicio, meio + 1, fim)
     }
+
     return - 1 // O valor não foi encontrado 
   }
+
+  let nums = [4, 16, 22, 29, 35, 44, 52, 66, 71, 80, 88, 94]
+
+  console.log(buscaBinaria(nums, 30, (elpos, busca) => {
+      if(busca === elPos) return 0
+      else if (busca < elPos) return -1
+      else return 1
+  }))
+
+ process.exit(0)
+ 
 
   const listaNomes = require('./dados/lista-nomes')
   console.time('FAUSTO')
@@ -60,4 +82,4 @@ function buscaBinaria(lista, valorBusca) {
   console.timeEnd('INSTRAGRAMILSON')
   console.log("Comparações: ", comp)
 
-}
+    }

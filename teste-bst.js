159eb3643e64d87d67c1042a2912ff33ab1e733f
DiegoPongeti1 <diego-p43@hotmail.com>
2020-11-26 12:34:00 +0000

    ( 26 / 11 )

--- teste-bst.js
+++ teste-bst.js
@@ -36,3 +36,47 @@ console.log('Mínimo: ', arvore.min())
 
 arvore.insert(5)
 console.log('Mínimo: ', arvore.min())
+
+console.log('Máximo: ', arvore.max())
+
+arvore.insert(66)
+console.log('Máximo: ', arvore.max())
+
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
+
+console.log('Existe 22?', arvore.search(22))
+console.log('Existe 49?', arvore.search(49))
+console.log('Existe 19?', arvore.search(19))
+
+arvore.insert(29)
+arvore.insert(40)
+arvore.insert(75)
+arvore.insert(78)
+arvore.insert(68)
+arvore.insert(1)
+
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
+
+arvore.remove(29) // Grau 0
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
+
+arvore.remove(66) // Grau 1 c/ direita
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
+
+arvore.remove(5) // Grau 1 c/ esquerda
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
+
+arvore.remove(31) // Grau 2 (raiz!!)
+vet = []
+arvore.inOrderTraversal(n => vet.push(n.data))
+console.log('Em-ordem', vet)
